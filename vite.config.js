f2f30eb62101fca1e4dd9fb5532224641f9c8f0b
Leonardo Messias <messiasleonardo98@gmail.com>
2022-04-04 09:57:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dynamicImportVariables from 'rollup-plugin-dynamic-import-variables'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        dynamicImportVariables()
+      ]
     }
   }
 })
